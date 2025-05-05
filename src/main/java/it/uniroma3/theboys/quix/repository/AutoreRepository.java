55f259288b3e854ca747c8b50bc2bf4787e5615f
Michela Sicuranza <michela.sicuranza@gmail.com>
2025-05-05 13:51:11 +0200

    aggiunta classi
    Aggiunta di classi repository e service per la classe autore

---
+++ src/main/java/it/uniroma3/theboys/quix/repository/AutoreRepository.java
@@ -0,0 +1,9 @@
+package it.uniroma3.theboys.quix.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.theboys.quix.model.Autore;
+
+public interface AutoreRepository extends CrudRepository<Autore, Long>{
+    
+}
