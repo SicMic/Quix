342d80fcde1873c4c767bc81f3dfba30404c30cf
Michela Sicuranza <michela.sicuranza@gmail.com>
2025-05-19 12:08:24 +0200

    modifica countdown quiz

--- src/main/resources/static/quizGame/script/main.js
+++ src/main/resources/static/quizGame/script/main.js
@@ -1,3 +1,5 @@
+let interruzioneCountdown = false
+
 document.addEventListener('click', function(e) {
     if(e.target.nodeName == "BUTTON") {
         const path = window.location.pathname
@@ -5,12 +7,13 @@ document.addEventListener('click', function(e) {
         const indiceQuiz = path.split('/')[3]
         const risposta = document.getElementById("answer").value
         let punteggio = parseInt(document.getElementById("score").innerText)
+        interruzioneCountdown = true
         if(risposta == e.target.value)
             document.getElementById("quiz-card").style.backgroundColor = "green" 
         else
             document.getElementById("quiz-card").style.backgroundColor = "red"
-        if(e.target.value == risposta) punteggio += 1 
-        sendAnswer(idRaccolta, indiceQuiz, punteggio)
+        if(e.target.value == risposta) punteggio += 1
+        setTimeout(()=> {sendAnswer(idRaccolta, indiceQuiz, punteggio)}, 1000) 
     }
 });
 
@@ -25,8 +28,11 @@ document.addEventListener("DOMContentLoaded", function() {
             const path = window.location.pathname
             sendAnswer(null, path.split('/')[2], path.split('/')[3])
         } else {
-            timeLeft--; // Decrementa il tempo rimanente
-            countdownElement.style.width = 100/30*timeLeft + "%"; // Aggiorna il contenuto con il tempo rimanente
+            if (!interruzioneCountdown){
+                timeLeft--; // Decrementa il tempo rimanente
+                countdownElement.style.width = 100/30*timeLeft + "%"; // Aggiorna il contenuto con il tempo rimanente
+            }
+
         }
     }, 1000); // Aggiorna ogni secondo (1000 millisecondi)
 });
